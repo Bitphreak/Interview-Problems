17fc6d176dde3acf7c5ef485b346a1443dc85fb0
Max J Spaulding - bitphreak <max.spaulding@razerzone.com>
2016-09-06 12:35:28 -0700

    Initial work on trei

---
+++ trei/trei.py
@@ -0,0 +1,27 @@
+
+class Trei(object) :
+
+    head = {}
+
+    def __init__(self, words=None) :
+        if words is not None :
+            for word in words :
+                self.insert_word(word)
+
+            print self.head
+
+    def insert_word(self, word) :
+        current = self.head
+        for i, char in word :
+            if not char in current :
+                current[char] = {}
+            current = current[char]
+
+
+if __name__ == '__main__':
+
+    with open('/usr/share/dict/words') as f :
+        words = f.read().splitlines()
+
+    trei = Trei(words)
+
